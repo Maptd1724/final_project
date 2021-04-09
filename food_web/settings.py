7e295a4036b8afdd0ee10ad5db625668a65f0953
Maptd1724 <tumamaodoitel@gmail.com>
2021-04-10 02:13:37 +0300

    set up for secret

--- food_web/settings.py
+++ food_web/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from secrets import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'th!&r+a&7-p^ccxhpc_-y8nqc*px%jyy5#gyf%z5m3%g0ds_p_'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+if DEBUG:
+    ALLOWED_HOSTS = []
+else:
+    ALLOWED_HOSTS = ['http://maryann1724.pythonanywhere.com/']
+
 
 
 # Application definition
